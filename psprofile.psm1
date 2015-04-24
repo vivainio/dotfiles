72fbec8aae58c7e75de2a80cebd10024e845e9d8
Ville M. Vainio <ville.vainio@basware.com>
2015-04-24 19:27:00 +0300

    "nice" tricks to force home directory

--- psprofile.psm1
+++ psprofile.psm1
@@ -1,3 +1,8 @@
+Remove-Variable -Force HOME -erroraction 'silentlycontinue'
+Remove-Item Env:\HOMEPATH
+Remove-Item Env:\HOMEDRIVE
+Set-Variable HOME "C:\Users\villevai"
+(get-psprovider 'FileSystem').Home = 'C:\Users\villevai'
 Set-Alias ccat pygmentize
 
 Export-ModuleMember -Alias *
