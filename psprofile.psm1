61b3d85ae4e8c89b3d7a6ffd0491f868b8c3296c
Ville M. Vainio <ville.vainio@basware.com>
2015-12-23 23:05:49 +0200

    'up' alias (cd ..)

--- psprofile.psm1
+++ psprofile.psm1
@@ -5,4 +5,8 @@ Set-Variable HOME "C:\Users\villevai"
 (get-psprovider 'FileSystem').Home = 'C:\Users\villevai'
 Set-Alias ccat pygmentize
 
+function go_up {Set-Location -Path ..}
+
+Set-Alias up go_up
 Export-ModuleMember -Alias *
+Export-ModuleMember go_up
