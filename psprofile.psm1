168154142f035a755e97635633269aaa01c1cf76
Ville M. Vainio <ville.vainio@basware.com>
2016-02-02 16:10:19 +0200

    add posh-git prompt

--- psprofile.psm1
+++ psprofile.psm1
@@ -1,3 +1,8 @@
+Import-Module PSReadline
+
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineOption -BellStyle Visual
+
 Remove-Variable -Force HOME -erroraction 'silentlycontinue'
 Remove-Item Env:\HOMEPATH
 Remove-Item Env:\HOMEDRIVE
@@ -10,4 +15,28 @@ function go_up {Set-Location -Path ..}
 Set-Alias up go_up
 Export-ModuleMember -Alias *
 Export-ModuleMember go_up
+
+Import-Module 'C:\Users\villevai\Documents\WindowsPowerShell\Modules\posh-git\posh-git'
+
 $GitPromptSettings.EnableFileStatus = $false
+
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    # Reset color, which can be messed up by Enable-GitColors
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+    Write-Host($pwd.ProviderPath) -nonewline -ForegroundColor Green
+
+    Write-VcsStatus
+
+    # Prompt on newline, with cmder colours.
+    Write-Host
+    Write-Host ">" -nonewline -ForegroundColor DarkGray
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return " "
+}
+
+# More posh-git init.
+Enable-GitColors
